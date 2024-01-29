3cbcac1fa2158155f1f6f6a5317e593b0713d5ad
Janet Zhong <91340575+janetzhong@users.noreply.github.com>
2024-01-29 15:07:37 -0800

    Edit home page, add readme

--- Home.py
+++ Home.py
@@ -15,15 +15,16 @@ st.markdown(f"[View Source Code]({source_code_url})", unsafe_allow_html=True)
 
 
 st.write("""
-## Discover Insights into Your Journal Entries
 
-This interactive platform offers a suite of tools designed to provide you with deeper insights into your personal journal entries. Whether you're looking to understand your emotional landscape over time or uncover the predominant themes occupying your thoughts, our analysis demos are here to guide you.
+This demo offers tools designed to provide you with deeper insights into your personal journal entries. It aims to help you understand your emotional landscape over time or uncover the predominant themes occupying your thoughts.
 
 ### How It Works
 
-- **Journal Emotion Radar with Huggingface**: Dive into the emotional depth of your journal entries. Our emotion analysis demo utilizes advanced natural language processing techniques to classify and visualize the emotions conveyed in your text.
+- 📡 **Journal Emotion Radar with Huggingface**: Gives you an emotional radar plot from an input journal entry of six emotions from huggingface model `michellejieli/emotion_text_classifier'.
 
-- **Brain Real Estate with Anthropic**: Explore the thematic distribution of your journal content. This demo parses your entries to identify and quantify the main themes, offering you a unique perspective on what occupies your mind space.
+- 🍰 **Brain Real Estate with Anthropic**: Categorizes an input journal entry into five main themes and plots % brain real estate for each theme in a pie chart.
+
+- 📈 **Multiple Journal Entry Analysis**: (coding in progress)
 
 👈  To get started, simply select a demo from the sidebar.
 """)
---
+++ README.md
@@ -0,0 +1,4 @@
+# Journal analysis demos
+
+CS224G sprint 1 demos on streamlit
+https://mindstorm-journal-analysis.streamlit.app/
